6abc46c739a156bc88ba4cf49385a01d479686c6
deifeb <119482274+deifeb@users.noreply.github.com>
2026-06-03 08:56:51 +0800

    Delete LCMA directory

--- LCMA/src/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-备件需求预测大模型系统
-"""
-
-__version__ = '1.0.0' 
--- LCMA/src/agents/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-备件需求预测智能体模块
-""" 
